797d656cea90254c8182d6f6a3cb616ef49071de
halimath <alexander.metzner@gmail.com>
2019-05-30 07:18:43 +0000

    Merged in feature/rollupjs (pull request #2)
    Feature/rollupjs
    
    * rollupjs config
    
    * rollup configuration
    
    * run build on pipeline

---
+++ rollup.config.js
@@ -0,0 +1,38 @@
+import typescript from "rollup-plugin-typescript2"
+import commonjs from "rollup-plugin-commonjs"
+
+// const pkg = require("./package.json")
+
+export default {
+    input: "./index.ts",
+
+    output: [
+        {
+            file: `dist/wecco-core.umd.js`,
+            name: "@wecco/core",
+            format: "umd",
+            sourcemap: true
+        },
+        {
+            file: `dist/wecco-core.es5.js`,
+            format: "es",
+            sourcemap: true
+        },
+    ],
+
+    // external: [
+    //     ...Object.keys(pkg.dependencies || {}),
+    //     ...Object.keys(pkg.peerDependencies || {}),
+    // ],
+
+    plugins: [
+        typescript({
+            tsconfigOverride: {
+                compilerOptions: {
+                    module: "ESNext",
+                },
+            },
+        }),
+        commonjs(),
+    ]
+}
