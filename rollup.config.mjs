79a73b10aadf110c897d78d7ee6936293cc0a2e3
Alexander Metzner <alexander.metzner@gmail.com>
2023-06-09 15:16:51 +0200

    build: add copyright notice to minified bundles

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,6 +1,10 @@
 import typescript from "@rollup/plugin-typescript"
 import commonjs from "@rollup/plugin-commonjs"
-import terser from '@rollup/plugin-terser'
+import terser from "@rollup/plugin-terser"
+
+import { readFileSync } from "fs"
+const { version } = JSON.parse(readFileSync("./package.json"))
+
 
 export default [
     {
@@ -36,14 +40,14 @@ export default [
 
         output: [
             {
-                file: "dist/weccoframework-core.js",
+                file: "dist/weccoframework-core.min.js",
                 format: "umd",
                 name: "wecco",
                 sourcemap: true,
             },
 
             {
-                file: "dist/weccoframework-core.mjs",
+                file: "dist/weccoframework-core.min.mjs",
                 name: "@weccoframework/core",
                 format: "es",
                 sourcemap: true,
@@ -57,7 +61,12 @@ export default [
                 },
             }),
             commonjs(),
-            terser(),
+            terser({
+                format: {
+                    preamble: `/* @weccoframework/core v${version}; Copyright 2019 - 2023 the wecco authors. Published under the terms of the Apache License V2. */`,
+                },
+            }),
+
         ]
     },
 
